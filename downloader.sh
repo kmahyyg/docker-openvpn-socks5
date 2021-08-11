7a3f5c0062ac385155ad1b46aa29ab1863e38fa7
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2021-08-11 15:23:29 +0000

    update build

--- downloader.sh
+++ downloader.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -v -e
+
 export LATEST_GOST_VER=$(curl --silent "https://api.github.com/repos/ginuerzh/gost/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' | cut -c 2-)
 curl -o gost.gz -sL https://github.com/ginuerzh/gost/releases/download/v${LATEST_GOST_VERSION}/gost-linux-amd64-${LATEST_GOST_VERSION}.gz 
 gunzip gost.gz 
